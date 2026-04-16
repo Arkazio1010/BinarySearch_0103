d9bff95915f3429f82e922f12be27fd1fbcb7ccc
Arkazio1010 <ziozafranesha@gmail.com>
2026-04-16 11:11:24 +0700

    melanjutkan void binarysearch lagi

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -94,4 +94,17 @@ void binarySearch()
         if (x > element[mid])
         {
           low = mid + 1;
-        
+        }
+
+      } while (low <= high);
+
+      if (low > high)
+      {
+        cout << "\n[!] Elemen " << x << " tidak ditemukan dalam array.\n";
+      }
+
+      cout << "\nIngin mencari lagi? (y/n): ";
+      cin >> ulang;
+
+  } while (ulang == 'y' || ulang == 'Y');
+}
