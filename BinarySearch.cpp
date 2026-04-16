fd84e57b96ff00e99aa2b0d2e08daf963a3bb9fa
Arkazio1010 <ziozafranesha@gmail.com>
2026-04-16 10:40:10 +0700

    melanjutkan void

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,6 +5,24 @@ int element[10];
 int nPanjang;
 int x;
 
+void input() 
+{
+    while (true)
+    {
+      cout << "Masukin banyaknya elemen pada array (maksimal 10):";
+      cin >> nPanjang;
+
+      if (nPanjang <= 10)
+      {
+     else
+      {
+        cout << "\n[!] jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n"; 
+      }
+    }
+    cout << "\n===================================\n";
+    cout << "        Memasukan Eleman Array        \n";
+    cout << "===================================\n";
 
   
+
 
