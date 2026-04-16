dfac6892df327252bcfe8d141aaebeb627d4797b
Arkazio1010 <ziozafranesha@gmail.com>
2026-04-16 11:05:39 +0700

    melanjutkan binarysearch

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -82,3 +82,16 @@ void binarySearch()
       {
        int mid = (low + high) / 2;
 
+       if (element[mid] == x)
+        {
+          cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+          return;
+        }
+        if (x < element[mid])
+        {
+          high = mid - 1;
+        }
+        if (x > element[mid])
+        {
+          low = mid + 1;
+        
